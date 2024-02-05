6ccf5734298ea8372fe8a9fbf71dab21f373e9a1
Anatoly Bugakov <anatoly.bugakov@outlook.com>
2024-02-05 18:32:01 +0000

    udfs sublib

---
+++ udfs/include/udfs/vector.hpp
@@ -0,0 +1,15 @@
+#ifndef __SUBLIB_1_H__
+#define __SUBLIB_1_H__
+
+class udfslib
+{
+public:
+    void print();
+};
+
+#endif
+
+struct Vector {
+    double* elem; // pointer to elements
+    int sz; // number of elements
+};
---
+++ udfs/src/udfs_main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "udfs/vector.hpp"
+
+void udfslib::print()
+{
+    std::cout << "Hello sub-library 1!" << std::endl;
+}
